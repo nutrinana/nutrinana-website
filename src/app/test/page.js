3f1900e4f98630b84840134efdc32eefbb72159d
Tanaya Patel <tanaya27@live.co.uk>
2025-03-15 20:08:09 +0800

    feat: NAG-25 collapsible timeline base created

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -1,16 +1,18 @@
-// import Navbar from "@/components/Navbar";
-// import Footer from "@/components/Footer";
-// import Banner from "@/components/Banner";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import Banner from "@/components/Banner";
+import Timeline from "@/components/Timeline";
 
-// export default function TestPage() {
-//   return (
-//     <div>
-//       <Banner />
-//       <Navbar />
-//       <main className="p-10">
-//         <h1 className="text-3xl font-bold">Component Preview</h1>
-//       </main>
-//       <Footer />
-//     </div>
-//   );
-// }
+export default function TestPage() {
+  return (
+    <div>
+      {/* <Banner /> */}
+      {/* <Navbar /> */}
+      <main className="p-10">
+        <h1 className="text-3xl font-bold">Component Preview</h1><br/>
+      <Timeline />
+      </main>
+      {/* <Footer /> */}
+    </div>
+  );
+}
