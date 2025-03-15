eedc60e958b9650b56e813716acc8b43ed0343ad
Tanaya Patel <tanaya27@live.co.uk>
2025-03-15 21:28:13 +0800

    feat: NAG-25 reusable component by specifying inputs

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -10,7 +10,36 @@ export default function TestPage() {
       {/* <Navbar /> */}
       <main className="p-10">
         <h1 className="text-3xl font-bold">Component Preview</h1><br/>
-      <Timeline />
+        <Timeline
+        timelineData={[
+            {
+                year: "2021",
+                text: "The magic begins! Nana perfects her activated granola recipe and starts selling it to her local neighbourhood.",
+                image: "https://placehold.co/200x216"
+            },
+            {
+                year: "2022",
+                text: "Nutrinana goes digital! The Activated Granola hits the virtual shelves on DELLI, making its way to kitchens across the UK.",
+                image: "https://placehold.co/200x216"
+            },
+            {
+                year: "2023",
+                text: "Nana joins forces with a group of green-thumbed garden enthusiasts to grow her own veggies in a shared allotment. Fresh, home-grown ingredients inspire new recipes, which she shares with her growing Instagram family.",
+                image: "https://placehold.co/200x216"
+            },
+            {
+                year: "2024",
+                text: "Nutrinana’s Activated Granola finds a home in the Black Farmers Shop in Brixton, bringing the taste of Nana’s kitchen to even more happy customers. This is only just the beginning!",
+                image: "https://placehold.co/200x216"
+            }
+            // ,
+            // {
+            //     year: "2025",
+            //     text: "Lorem Ipsum Dolor Sit Amet",
+            //     image: "https://placehold.co/200x216"
+            // }
+        ]}
+        />  
       </main>
       {/* <Footer /> */}
     </div>
