0312ca6ae021362ae4fa246c33700d618f6043e4
Tanaya Patel <tanaya27@live.co.uk>
2025-03-22 19:43:36 +0800

    feat: NAG-25 final code cleanup

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -15,22 +15,22 @@ export default function TestPage() {
             {
                 year: "2021",
                 text: "The magic begins! Nana perfects her activated granola recipe and starts selling it to her local neighbourhood.",
-                image: "/timeline/first_batch.jpg"
+                image: "/timeline/2021.jpg"
             },
             {
                 year: "2022",
                 text: "Nutrinana goes digital! The Activated Granola hits the virtual shelves on DELLI, making its way to kitchens across the UK.",
-                image: "/timeline/DELLI_package.jpeg"
+                image: "/timeline/2022.jpeg"
             },
             {
                 year: "2023",
                 text: "Nana joins forces with a group of green-thumbed garden enthusiasts to grow her own veggies in a shared allotment. Fresh, home-grown ingredients inspire new recipes, which she shares with her growing Instagram family.",
-                image: "/timeline/allotment.jpg"
+                image: "/timeline/2023.jpg"
             },
             {
                 year: "2024",
                 text: "Nutrinana’s Activated Granola finds a home in the Black Farmers Shop in Brixton, bringing the taste of Nana’s kitchen to even more happy customers. This is only just the beginning!",
-                image: "/timeline/TBF_shelf.jpeg"
+                image: "/timeline/2024.jpeg"
             }
             // ,
             // {
