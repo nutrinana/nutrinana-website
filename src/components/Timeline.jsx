830fd18b82f3d41c35ff70251403d835cf13e8b8
Tanaya Patel <tanaya27@live.co.uk>
2025-03-22 16:17:54 +0800

    feat: NAG-25 dynamic sizing of elements

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -16,26 +16,34 @@ export default function Timeline({ timelineData }) {
     } = useTimeline(timelineData);
 
     return (
-        <div className="relative flex justify-center items-center w-full h-full overflow-hidden transition-all duration-500" style={{ height: isOpen ? `${expandedHeight}px` : `${collapsedHeight}px` }}>
+        <div className="relative flex justify-center items-center w-full h-full overflow-visible transition-all duration-500" style={{ height: isOpen ? `${expandedHeight}px` : `${collapsedHeight}px` }}>
             {/* Collapsible Trigger covering the entire component */}
             <Collapsible open={isOpen} onOpenChange={setIsOpen}>
                 <CollapsibleTrigger asChild>
-                    <div className="absolute inset-0 z-10 cursor-pointer opacity-0"></div>
+                    <div>
+                        <div className="absolute inset-0 z-10 cursor-pointer opacity-0"></div>
+                        <div className="absolute text-3xl z-10 cursor-pointer top-[420px] hover:text-[#507153]">
+                            <div className="absolute right-[50%] px-1">{isOpen ? '' : 'See '}</div>
+                            <div className="absolute left-[50%] px-1">{isOpen ? '' : ' More...'}</div>
+                        </div>
+                    </div>
+                    
                 </CollapsibleTrigger>
                 <CollapsibleContent className={isOpen ? 'collapsible-content-enter' : 'collapsible-content-exit'}>
                     {/* Collapsible Timeline Slice */}
                     <div className="absolute left-1/2 h-0 origin-top-left -rotate-90 outline-3 outline-offset-[-1.5px] outline-[#507153]" style={{ width: `${200 + (timelineData.length - 2) * 300}px`, top: `${240 + (timelineData.length - 1) * 300}px` }} />
+                    {/* Timeline items */}
                     {timelineData.slice(2).map((item, index) => (
-                        <div key={index} className={`absolute inline-flex flex-col items-center gap-2.5 w-40 ${index % 2 === 0 ? 'right-1/2' : 'left-1/2'}`} style={{ top: `${600 + index * 300}px` }}>
-                            <div className="self-stretch h-10 text-center text-2xl font-semibold text-zinc-900">
+                        <div key={index} className={`absolute inline-flex flex-col items-center gap-2.5 w-[40%] ${index % 2 === 0 ? 'right-1/2' : 'left-1/2'}`} style={{ top: `${630 + index * 300}px` }}>
+                            <div className={`absolute w-full h-auto top-[-40px] ${index % 2 === 0 ? 'text-right' : 'text-left'} px-5 text-2xl font-semibold text-zinc-900`}>
                                 {item.year}
                             </div>
-                            <div className={`w-40 h-0 origin-top-${index % 2 === 0 ? 'right' : 'left'} outline-3 outline-offset-[-1.5px] outline-[#507153]`} />
-                            <div className={`absolute w-6 h-6 bg-[#507153] rounded-full top-[38px] ${index % 2 === 0 ? 'left-[-10px]' : 'right-[-10px]'}`}></div>
-                            <div className={`absolute w-[300px] h-32 text-xl font-normal text-black ${index % 2 === 0 ? 'left-[-160px] text-right' : 'left-[20px] text-left'} top-[70px]`}>
+                            <div className={`absolute w-40 h-0 ${index % 2 === 0 ? 'right-[0]' : 'left-[0]'} top-[100%] outline-3 outline-offset-[-1.5px] outline-[#507153]`} />
+                            <div className={`absolute w-6 h-6 bg-[#507153] rounded-full top-[-12px] ${index % 2 === 0 ? 'right-[140px]' : 'left-[140px]'}`}></div>
+                            <div className={`absolute w-full h-auto font-normal text-black ${index % 2 === 0 ? 'left-[-20px] text-right' : 'left-[20px] text-left'} top-[20px]`} style={{ display: '-webkit-box', WebkitBoxOrient: 'vertical', overflow: 'hidden', fontSize: 'var(--dynamic-font-size)' }}>
                                 {item.text}
                             </div>
-                            <img className={`absolute w-48 h-48 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] border-6 border-white object-cover ${index % 2 === 0 ? 'left-[260px] rotate-[-13deg]' : 'left-[-260px] rotate-[15deg]'}`} src={item.image} />
+                            <img className={`absolute w-full h-auto max-w-[200px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] border-6 border-white object-cover top-[-40px] ${index % 2 === 0 ? 'left-[120%] rotate-[-13deg]' : 'right-[120%] rotate-[15deg]'}`} src={item.image} />
                         </div>
                     ))}
                     {/* bottom circle */}
@@ -45,31 +53,31 @@ export default function Timeline({ timelineData }) {
 
             {/* Initial Timeline Slice */}
             <div className="absolute left-1/2 top-[520px] h-0 origin-top-left -rotate-90 outline-3 outline-offset-[-1.5px] outline-[#507153]" style={{ width: '520px' }} />
-            <div className="absolute inline-flex flex-col items-center gap-2.5 w-40 right-1/2 top-[40px]">
-                <div className="self-stretch h-10 text-2xl font-semibold text-center text-zinc-900">
+            <div className="absolute inline-flex flex-col items-center gap-2.5 w-[40%] right-1/2 top-[40px]">
+                <div className="absolute w-full top-[-40px] text-right px-5 text-2xl font-semibold text-zinc-900">
                     {timelineData[0].year}
                 </div>
-                <div className="w-40 h-0 origin-top-left outline-3 outline-offset-[-1.5px] outline-[#507153]" />
-                <div className="absolute w-6 h-6 bg-[#507153] rounded-full top-[38px] left-[-10px]"></div>
-                <div className="absolute w-[300px] h-20 text-xl font-normal text-right text-black left-[-160px] top-[70px]">
+                <div className="absolute w-40 h-0 right-[0] top-[100%] outline-3 outline-offset-[-1.5px] outline-[#507153]" />
+                <div className="absolute w-6 h-6 bg-[#507153] rounded-full top-[-12px] right-[140px]"></div>
+                <div className="absolute w-full h-auto text-xl font-normal text-black left-[-20px] text-right top-[20px]" style={{ display: '-webkit-box', WebkitBoxOrient: 'vertical', overflow: 'hidden', fontSize: 'var(--dynamic-font-size)' }}>
                     {timelineData[0].text}
                 </div>
-                <img className="absolute w-48 h-48 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] border-6 border-white object-cover left-[260px] top-[-20px] rotate-[-13deg]" src={timelineData[0].image} />
+                <img className="absolute w-full h-auto max-w-[200px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] border-6 border-white object-cover top-[-40px] left-[120%] rotate-[-13deg]" src={timelineData[0].image} />
             </div>
             {/* top circle */}
-            <div className="absolute w-6 h-6 bg-[#507153] rounded-full top-[0px]" style={{ left: 'calc(50% - 12px)' }}></div>
+            <div className="absolute w-6 h-6 bg-[#507153] rounded-full top-[-5px]" style={{ left: 'calc(50% - 12px)' }}></div>
 
             {/* Second Timeline Slice */}
-            <div className="absolute inline-flex flex-col items-center gap-2.5 w-40 left-1/2 top-[330px]">
-                <div className="self-stretch h-10 text-2xl font-semibold text-center text-zinc-900">
+            <div className="absolute inline-flex flex-col items-center gap-2.5 w-[40%] left-1/2 top-[330px]">
+                <div className="absolute w-full top-[-40px] text-left px-5 text-2xl font-semibold text-zinc-900">
                     {timelineData[1].year}
                 </div>
-                <div className="w-40 h-0 origin-top-right outline-3 outline-offset-[-1.5px] outline-[#507153]" />
-                <div className="absolute w-6 h-6 bg-[#507153] rounded-full top-[38px] right-[-10px]"></div>
-                <div className="absolute w-[300px] h-20 text-xl font-normal text-left text-black left-[20px] top-[70px]">
+                <div className="absolute w-40 h-0 left-[0] top-[100%] outline-3 outline-offset-[-1.5px] outline-[#507153]" />
+                <div className="absolute w-6 h-6 bg-[#507153] rounded-full top-[-12px] left-[140px]"></div>
+                <div className="absolute w-full h-auto text-xl font-normal text-black left-[20px] text-left top-[20px]" style={{ display: '-webkit-box', WebkitBoxOrient: 'vertical', overflow: 'hidden', fontSize: 'var(--dynamic-font-size)' }}>
                     {timelineData[1].text}
                 </div>
-                <img className="absolute w-48 h-48 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] border-6 border-white object-cover left-[-260px] top-[-20px] rotate-[15deg]" src={timelineData[1].image} />
+                <img className="absolute w-full h-auto max-w-[200px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] border-6 border-white object-cover top-[-40px] right-[120%] rotate-[15deg]" src={timelineData[1].image} />
             </div>
 
             {/* Blurred Overlay for Second Timeline Slice */}
@@ -82,7 +90,6 @@ export default function Timeline({ timelineData }) {
                     }}>
                 </div>
             )}
-            <div className="absolute text-3xl top-[420px]" style={{ background: isOpen ? '' : 'white' }}>{isOpen ? '' : 'See More'}</div>
         </div>
     );
 }
