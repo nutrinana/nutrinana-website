ee4109b25220ac29262f39befd31df38f84e802b
Tanaya Patel <tanaya27@live.co.uk>
2025-03-22 19:47:38 +0800

    feat: NAG-25 final commit

--- src/hooks/useTimeline.js
+++ src/hooks/useTimeline.js
@@ -6,15 +6,10 @@ export default function useTimeline(timelineData) {
     const collapsedHeight = 540;
     const expandedHeight = useMemo(() => 600 + (timelineData.length - 2) * 300, [timelineData.length]);
 
-    const finalSliceTop = useMemo(() => 300 + (timelineData.length - 2) * 300, [timelineData.length]);
-    const bottomCircleTop = useMemo(() => 540 + (timelineData.length - 2) * 300, [timelineData.length]);
-
     return {
         isOpen,
         setIsOpen,
         collapsedHeight,
         expandedHeight,
-        finalSliceTop,
-        bottomCircleTop,
     };
 }
