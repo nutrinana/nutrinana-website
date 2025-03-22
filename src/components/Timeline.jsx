543a9f36ce0a469acac94b731e0f32d21ae136fa
Ruben Odamo <psyro3@exmail.nottingham.ac.uk>
2025-03-22 11:23:57 +0000

    feat: NAG-25 rounded end timeline

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -9,13 +9,13 @@ import useTimeline from "@/hooks/useTimeline";
 const TimelineItem = ({ item, index }) => (
     <div key={index} className={`absolute inline-flex flex-col items-center gap-2.5 w-[40%] ${index % 2 === 0 ? 'right-1/2' : 'left-1/2'}`} 
     style={{ top: `${30 + index * 300}px` }}>
-        <div className={`absolute w-full h-auto top-[-40px] ${index % 2 === 0 ? 'text-right' : 'text-left'} px-5 text-2xl font-semibold text-zinc-900`}>
+        <div className={`absolute w-full h-auto top-[-40px] ${index % 2 === 0 ? 'text-right' : 'text-left'} px-5 text-3xl font-semibold text-zinc-900`}>
             {item.year}
         </div>
         <div className={`absolute w-40 h-0 ${index % 2 === 0 ? 'right-[0]' : 'left-[0]'} top-[100%] outline-3 outline-offset-[-1.5px] outline-[#507153]`} />
         <div className={`absolute w-6 h-6 bg-[#507153] rounded-full top-[-12px] ${index % 2 === 0 ? 'right-[140px]' : 'left-[140px]'}`}></div>
         <div className={`absolute w-full h-auto font-normal text-black ${index % 2 === 0 ? 'left-[-20px] text-right' : 'left-[20px] text-left'} top-[20px]`} 
-        style={{ display: '-webkit-box', WebkitBoxOrient: 'vertical', overflow: 'hidden', fontSize: 'var(--dynamic-font-size)' }}>
+        style={{ display: 'block', fontSize: 'var(--dynamic-font-size)' }}>
             {item.text}
         </div>
         <img className={`absolute w-full h-auto max-w-[200px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] border-6 border-white object-cover top-[-40px] 
@@ -29,12 +29,15 @@ export default function Timeline({ timelineData }) {
         setIsOpen,
         collapsedHeight,
         expandedHeight,
-        bottomCircleTop,
     } = useTimeline(timelineData);
 
     return (
         <div className={`relative flex justify-center items-center w-full h-full overflow-visible transition-all duration-500`} 
         style={{ height: isOpen ? `${expandedHeight}px` : `${collapsedHeight}px` }}>
+            <div
+                className="absolute left-1/2 w-[6px] bg-[#507153] rounded-full transform -translate-x-1/2 transition-all duration-500"
+                style={{ height: isOpen ? `${expandedHeight}px` : `${collapsedHeight}px`, top: 0 }}
+            />
             <Collapsible open={isOpen} onOpenChange={setIsOpen}>
                 <CollapsibleTrigger asChild>
                     <div>
@@ -46,18 +49,12 @@ export default function Timeline({ timelineData }) {
                     </div>
                 </CollapsibleTrigger>
                 <CollapsibleContent className={isOpen ? 'collapsible-content-enter' : 'collapsible-content-exit'}>
-                    <div className="absolute left-1/2 h-0 origin-top-left -rotate-90 outline-3 outline-offset-[-1.5px] outline-[#507153]" 
-                    style={{ width: `${200 + (timelineData.length - 2) * 300}px`, top: `${240 + (timelineData.length - 1) * 300}px` }} />
                     {timelineData.slice(2).map((item, index) => (
                         <TimelineItem key={index + 2} item={item} index={index + 2} />
                     ))}
-                    <div className="absolute w-6 h-6 bg-[#507153] rounded-full transition-all duration-500" 
-                    style={{ top: `${bottomCircleTop}px`, left: 'calc(50% - 12px)' }}></div>
                 </CollapsibleContent>
             </Collapsible>
 
-            <div className="absolute left-1/2 top-[520px] h-0 origin-top-left -rotate-90 outline-3 outline-offset-[-1.5px] outline-[#507153]" 
-            style={{ width: '520px' }} />
             <TimelineItem item={timelineData[0]} index={0} />
             <div className="absolute w-6 h-6 bg-[#507153] rounded-full top-[-5px]" style={{ left: 'calc(50% - 12px)' }}></div>
             <TimelineItem item={timelineData[1]} index={1} />
@@ -68,4 +65,3 @@ export default function Timeline({ timelineData }) {
         </div>
     );
 }
-
