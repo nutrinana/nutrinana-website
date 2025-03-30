b33d98bb35916bc0d6e3d35606bcf2f97b43954e
Ruben <93412774+rubenodamo@users.noreply.github.com>
2025-03-30 23:06:29 +0100

    feat: NAG-36 moved helper function to utils file

--- src/components/GlobalAccordion.jsx
+++ src/components/GlobalAccordion.jsx
@@ -13,14 +13,17 @@ export default function GlobalAccordion({ items }) {
             {items.map((item, index) => (
                 <AccordionItem value={`item-${index}`} key={index}>
                     <AccordionTrigger className="text-xl font-bold font-heading text-left">
+                        {/* Accordion header */}
                         {item.title}
                     </AccordionTrigger>
                     <AccordionContent className="text-base font-body text-black space-y-4">
+                        {/* Optional subtitle, displayed when open */}
                         {item.subtitle && (
                             <div className="text-lg font-bold font-heading text-black">
                                 {item.subtitle}
                             </div>
                         )}
+                        {/* Accordion content */}
                         <div>{item.content}</div>
                     </AccordionContent>
                 </AccordionItem>
