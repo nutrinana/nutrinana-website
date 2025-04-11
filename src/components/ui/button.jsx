ab1bfaa338d6866358c922f20c60d725ad6b2b4e
Ruben <93412774+rubenodamo@users.noreply.github.com>
2025-04-12 00:03:16 +0100

    feat: NAG-34 removed comment

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -27,7 +27,6 @@ const buttonVariants = cva(
           "border border-[var(--color-raisin)] bg-[var(--color-grey)] text-[var(--color-raisin)] shadow-xs transition-transform duration-250 ease-in-out hover:scale-110 hover:border-2",
         outline:
           "border border-input bg-background shadow-xs hover:bg-accent hover:text-accent-foreground",
-          // "border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
         ghost: 
