f29c9d42e05353b3106aa07b09216bfe75b7d471
Ruben <93412774+rubenodamo@users.noreply.github.com>
2025-04-21 17:09:29 +0100

    bugfix: NAG-47 image index fix

--- src/app/robots.js
+++ src/app/robots.js
@@ -5,7 +5,6 @@
  * 
  * @returns {Object} - The robots.txt configuration object.
  */
-// const isProduction = process.env.NODE_ENV === "production";
 const isProduction = process.env.STAGE === "production";
 
 export default function robots() {
