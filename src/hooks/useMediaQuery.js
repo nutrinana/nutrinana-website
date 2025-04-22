5c6cf7e3c04d9f89bf36eab0647419ec986e6dac
Ruben <93412774+rubenodamo@users.noreply.github.com>
2025-04-22 22:47:31 +0100

    feat: NAG-48 commented files

--- src/hooks/useMediaQuery.js
+++ src/hooks/useMediaQuery.js
@@ -1,5 +1,13 @@
 import { useState, useEffect } from "react";
 
+/**
+ * Custom hook to check if a media query matches.
+ * It listens for changes in the media query and updates the state accordingly.
+ * Use this hook to conditionally render components based on screen size or other media features.
+ * 
+ * @param {string} query - The media query string to match against.
+ * @returns {boolean} - `true` if the media query matches, false otherwise.
+ */
 export function useMediaQuery(query) {
   const [matches, setMatches] = useState(false);
 
