d0680bcea4f427bc2a7fa0e5c2623561642fa685
Ruben Odamo <psyro3@exmail.nottingham.ac.uk>
2025-04-29 23:06:10 +0100

    feat: NAG-32 added dynamic title

--- src/app/legal/layout.js
+++ src/app/legal/layout.js
@@ -1,10 +1,10 @@
-export const metadata = {
-    title: "Legal - Nutrinana",
-};
+"use client";
+
+import { usePathname } from "next/navigation";
 
 /**
  * This component serves as a layout for the legal pages of the Nutrinana website.
- * It includes a title and navigation links to the different legal pages.
+ * It includes a dynamic title and navigation links to the different legal pages.
  * 
  * @param {object} props - The properties passed to the component.
  * @param {React.ReactNode} props.children - The child components to be rendered within the layout.
@@ -12,9 +12,15 @@ export const metadata = {
  * @returns {JSX.Element} The layout for the legal pages.
  */
 export default function LegalLayout({ children }) {
+    const pathname = usePathname();
+
+    let heading = "Terms and Conditions"; // Default
+    if (pathname.includes("privacy-policy")) heading = "Privacy Policy";
+    else if (pathname.includes("cookie-policy")) heading = "Cookie Policy";
+
     return (
         <div className="max-w-7xl mx-auto py-12">
-            <h1 className="text-3xl font-bold text-center mb-6">Terms and Conditions</h1>
+            <h1 className="text-3xl font-bold text-center mb-6">{heading}</h1>
             <div className="flex justify-center gap-12 mb-10 text-md underline text-gray-700">
                 <a href="/legal">Website Terms of Use</a>
                 <a href="/legal/privacy-policy">Privacy Policy</a>
