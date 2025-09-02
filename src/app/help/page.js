c2448a443e2fd3ccecec2a91cf017bcdbfa2b527
Ruben Odamo <psyro3@exmail.nottingham.ac.uk>
2025-09-02 19:15:48 +0100

    feat: NAG-60 update text to use typographic quote marks

--- src/app/help/page.js
+++ src/app/help/page.js
@@ -33,21 +33,21 @@ const accordionData = [
     },
     {
         title: "Allergens",
-        subtitle: "Is Nutrinana's Activated Granola suitable for people with allergies?",
+        subtitle: "Is Nutrinana’s Activated Granola suitable for people with allergies?",
         content:
             "Our granola is made with a variety of nuts and seeds, so it may not be suitable for those with nut or seed allergies. We take great care to avoid cross-contamination, but we recommend that anyone with severe allergies consult the ingredient list carefully and reach out to us with any concerns. Your health and safety are our top priority!",
     },
     {
-        title: 'What Does "Activated" Mean?',
+        title: "What Does ‟Activated” Mean?",
         subtitle: 'What does it mean when we say our granola is "Activated"?',
         content:
-            '"Activated" means that the nuts and seeds in our granola are soaked and gently dried as part of a traditional preparation method. This process is often used to mirror the natural sprouting conditions of seeds and is valued for its thoughtful approach to ingredient preparation. At Nutrinana, it’s one of the ways we give extra care to what goes into every batch, contributing to the unique texture and flavour of our granola.',
+            "‟Activated” means that the nuts and seeds in our granola are soaked and gently dried as part of a traditional preparation method. This process is often used to mirror the natural sprouting conditions of seeds and is valued for its thoughtful approach to ingredient preparation. At Nutrinana, it’s one of the ways we give extra care to what goes into every batch, contributing to the unique texture and flavour of our granola.",
     },
     {
         title: "Storage and Date",
-        subtitle: "How should I store my Nutrinana's Activated Granola, and how long will it last?",
+        subtitle: "How should I store my Nutrinana’s Activated Granola, and how long will it last?",
         content:
-            "To keep your granola fresh and crunchy, store it in an airtight container in a cool, dry place. Our granola has a shelf life of 6-12 months if stored properly—but let's be honest, it'll probably last less than that because it's just that tasty! You'll find the best-before date printed on the package, so you can enjoy every bite at its peak. Remember to reseal the packaging after each use to maintain its freshness!",
+            "To keep your granola fresh and crunchy, store it in an airtight container in a cool, dry place. Our granola has a shelf life of 6-12 months if stored properly—but let’s be honest, it’ll probably last less than that because it’s just that tasty! You’ll find the best-before date printed on the package, so you can enjoy every bite at its peak. Remember to reseal the packaging after each use to maintain its freshness!",
     },
 ];
 
