f122a5976a9784df65ffdf0eb5e0a5c3baf5fd3d
Ruben <93412774+rubenodamo@users.noreply.github.com>
2025-09-09 16:26:17 +0100

    feat: NAG-68 add closing CTA to reviews page

--- src/app/reviews/page.js
+++ src/app/reviews/page.js
@@ -3,6 +3,7 @@
 
 import dynamic from "next/dynamic";
 
+import ClosingCTA from "@/components/ClosingCTA";
 import LeaveReviewForm from "@/components/LeaveReviewForm";
 import RecentReviewCards from "@/components/RecentReviewCards";
 
@@ -10,19 +11,27 @@ const YotpoSEOProductWidget = dynamic(() => import("@/components/YotpoSEOProduct
     ssr: false,
 });
 
-const YotpoReviewWidget = dynamic(() => import("@/components/YotpoReviewWidget"), { ssr: false });
+const mainProduct = {
+    productId: "activated-granola",
+    name: "Nutrinana's Activated Granola",
+    url: "https://www.nutrinana.co.uk/activated-granola",
+    imageUrl: "https://www.nutrinana.co.uk/products/mixed-fruits/granola1.jpg",
+    price: 8.5,
+    currency: "GBP",
+    description: "A delicious and nutritious base granola for all flavours.",
+};
+
+const closingCTAData = {
+    header: "Ready to taste the granola everyone’s raving about?",
+    description: "Join our happy customers and try Nutrinana’s Activated Granola for yourself.",
+    button: {
+        text: "Shop Activated Granola",
+        href: "/activated-granola",
+        variant: "green",
+    },
+};
 
 export default function ReviewsPage() {
-    const mainProduct = {
-        productId: "activated-granola",
-        name: "Nutrinana's Activated Granola",
-        url: "https://www.nutrinana.co.uk/activated-granola",
-        imageUrl: "https://www.nutrinana.co.uk/products/mixed-fruits/granola1.jpg",
-        price: 8.5,
-        currency: "GBP",
-        description: "A delicious and nutritious base granola for all flavours.",
-    };
-
     return (
         <div className="site-container">
             <section className="section-y">
@@ -34,13 +43,17 @@ export default function ReviewsPage() {
                 <LeaveReviewForm productId={mainProduct.productId} />
             </section>
 
-            <section className="section-y">
+            <section className="section-y:last-child">
                 <h2 id="more-reviews" className="h2 mb-6">
                     More Reviews
                 </h2>
                 <div className="max-w-400">
                     <YotpoSEOProductWidget />
                 </div>
+            </section>
+
+            <section className="section-y:last-child">
+                <ClosingCTA {...closingCTAData} />
             </section>
         </div>
     );
