6468b7aeecce001be5b4173cf36fd60d941ea19e
Ruben <93412774+rubenodamo@users.noreply.github.com>
2025-09-10 16:39:28 +0100

    feat: NAG-46 update comments for client wrappers

--- src/components/YotpoClientWrapper.jsx
+++ src/components/YotpoClientWrapper.jsx
@@ -5,7 +5,13 @@ import dynamic from "next/dynamic";
 const YotpoReviewWidget = dynamic(() => import("@/components/YotpoReviewWidget"), { ssr: false });
 
 /**
- * Client-side wrapper for YotpoReviewWidget to enable ssr: false in server components.
+ * Client-side wrapper for @see {@link YotpoReviewWidget} to enable ssr: false in server components.
+ *
+ * @component
+ *
+ * @param {Object} props - The properties for the YotpoClientWrapper component.
+ *
+ * @returns {JSX.Element} The rendered YotpoClientWrapper component.
  */
 export default function YotpoClientWrapper(props) {
     return <YotpoReviewWidget {...props} />;
