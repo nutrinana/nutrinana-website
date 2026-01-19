2c77ab0beedcb1ef57e191bbf0f79a90a91dcedb
Ruben <93412774+rubenodamo@users.noreply.github.com>
2026-01-19 23:25:00 +0000

    feat: NAG-82 change icon stroke width

--- src/components/CartButton.jsx
+++ src/components/CartButton.jsx
@@ -21,7 +21,7 @@ export default function CartButton({ onClick }) {
 
     return (
         <Link href="/basket" onClick={onClick} className="social-icon" aria-label="Open basket">
-            <ShoppingBasket size={24} />
+            <ShoppingBasket size={24} strokeWidth={1.5} />
             {itemCount > 0 && (
                 <span className="absolute -top-2 -right-2 rounded-full bg-black px-1.5 py-0.5 text-[10px] font-semibold text-white">
                     {itemCount}
