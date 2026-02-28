1d46dfeb8f83fcd6eb50463b8c2b175a6b8a7e6e
Ruben <93412774+rubenodamo@users.noreply.github.com>
2026-02-28 17:42:13 +0000

    bugfix: move provider inside body to fix hydration issue

--- src/app/layout.js
+++ src/app/layout.js
@@ -95,24 +95,24 @@ export default function RootLayout({ children }) {
     const yotpoStoreId = process.env.NEXT_PUBLIC_YOTPO_STORE_ID;
 
     return (
-        <ClerkProvider>
-            <html
-                lang="en"
-                className={`${montserrat.variable} ${playfairDisplay.variable} ${tanNimbus.variable} ${feelingPassionate.variable}`}
-            >
-                <head>
-                    {/* Yotpo Reviews Loader Script */}
-                    {yotpoStoreId && (
-                        <script
-                            id="yotpo-loader"
-                            src={`https://cdn-widgetsrepository.yotpo.com/v1/loader/${yotpoStoreId}`}
-                            type="text/plain"
-                            data-cookieconsent="statistics, marketing"
-                            async
-                        />
-                    )}
-                </head>
-                <body className="flex min-h-screen flex-col overflow-x-hidden bg-white text-gray-900">
+        <html
+            lang="en"
+            className={`${montserrat.variable} ${playfairDisplay.variable} ${tanNimbus.variable} ${feelingPassionate.variable}`}
+        >
+            <head>
+                {/* Yotpo Reviews Loader Script */}
+                {yotpoStoreId && (
+                    <script
+                        id="yotpo-loader"
+                        src={`https://cdn-widgetsrepository.yotpo.com/v1/loader/${yotpoStoreId}`}
+                        type="text/plain"
+                        data-cookieconsent="statistics, marketing"
+                        async
+                    />
+                )}
+            </head>
+            <body className="flex min-h-screen flex-col overflow-x-hidden bg-white text-gray-900">
+                <ClerkProvider>
                     <CartProvider>
                         {/* Cookie Banner */}
                         <CookieBotConsent />
@@ -134,8 +134,8 @@ export default function RootLayout({ children }) {
                         {/* Toaster for notifications */}
                         <Toaster position="bottom-right" richColors />
                     </CartProvider>
-                </body>
-            </html>
-        </ClerkProvider>
+                </ClerkProvider>
+            </body>
+        </html>
     );
 }
