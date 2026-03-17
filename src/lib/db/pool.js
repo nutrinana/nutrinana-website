626d02a7ce72af5e2086cccbed42bd6e91da71f9
Ruben <93412774+rubenodamo@users.noreply.github.com>
2026-03-17 19:41:11 +0000

    further debugging for CA

--- src/lib/db/pool.js
+++ src/lib/db/pool.js
@@ -12,5 +12,8 @@ import { Pool } from "pg";
 export const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
 
-    ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+    ssl:
+        process.env.NODE_ENV === "production"
+            ? { rejectUnauthorized: false, checkServerIdentity: () => undefined }
+            : false,
 });
